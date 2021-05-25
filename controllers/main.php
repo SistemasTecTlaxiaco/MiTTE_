2e63966961ae7bb0336bb9f51a581caf9994365b
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-05-25 16:46:00 -0500

    Update main.php
    Actualización controllers Main

--- controllers/main.php
+++ controllers/main.php
@@ -1,41 +1,37 @@
 <?php
 include_once 'models/consultamodel.php';
+
 class Main extends Controller{
 
     function __construct(){
         parent::__construct();
-     
-        //echo "<p>Nuevo controlador Main</p>";
-    }
+     }
     function render(){
         $this->view->render('main/index');
     }
-    function saludo(){
-        echo "<p>Ejecutaste el método Saludo</p>";
-    }
-
     function login(){
     $userSession = new UserSession();
-      $user = new User();
+    $user = new User();
      if(isset($_SESSION['user'])){
     //echo "Hay sesion";
     $user->setUser($userSession->getCurrentUser());
-     include_once '/views/main/home.php';
+
+     include_once '/views/main/index.php';
      }else if(isset($_POST['email']) && isset($_POST['pass'])){
 
  // echo "validacion de login";
- $correoForm = $_POST['email'];
- $contraseñaForm = $_POST['pass'];
+ $userForm = $_POST['email'];
+ $passForm = $_POST['pass'];
 
- if($user->userExists($correoForm, $contraseñaForm)){
+ if($user->userExists($userForm, $passForm)){
   // echo "usuario validado";
-  $userSession->setCurrentUser($correoForm);
-  $user->setUser($correoForm);
+  $userSession->setCurrentUser($userForm);
+  $user->setUser($userForm);
    
-   include_once 'views/main/home.php';
+   include_once 'views/principal/index.php';
  }else{
   // echo "nombre de usuario y/o password incorrecto";
- $errorLogin = "Correo de usuario y/o password incorrecto";
+ $errorLogin = "Nombre de usuario y/o password es incorrecto";
  include_once 'views/main/index.php';
 }
 }else{
