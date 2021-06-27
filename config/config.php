c410e962a63276e11fd82701480dfb3976556619
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-06-27 00:55:23 -0500

    Actualización de valores de las variables de conexión

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 
 define('URL', 'https://sistemamitte.herokuapp.com/');
-define('HOST', 'localhost');
-define('DB', 'mytte');
-define('USER', 'root');
-define('PASSWORD', "JgSyLaP97");
+define('HOST', 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB', 'yi2uha8khxcayus4');
+define('USER', 'clzympj6ook1tia1');
+define('PASSWORD', "opboggddikaapaec");
 define('CHARSET', 'utf8mb4');
 
 ?>
