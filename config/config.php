575becfc6c92bf6ead35042951436d701b3f05f5
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-06-27 01:29:00 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 define('URL', 'https://sistemamitte.herokuapp.com/');
 define('HOST', 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 define('DB', 'yi2uha8khxcayus4');
-define('USER', 'root');
+define('USER', 'clzympj6ook1tia1');
 define('PASSWORD', "opboggddikaapaec");
 define('CHARSET', 'utf8mb4');
 
