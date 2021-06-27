8aad34e1a6e332e504fc16f98c3366c6bfaaf70a
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-06-27 01:19:52 -0500

    Correccion de error imagen "librería" en misArchivos

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
 require_once 'libs/database.php';
 require_once 'libs/model.php';
 require_once 'libs/controller.php';
@@ -10,6 +11,9 @@ require_once 'libs/app.php';
 include_once 'includes/user_session.php';
 include_once 'includes/sessionController.php';
 require_once 'config/config.php';
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 $app = new App();
 
 $nemail = new PHPMailer(true);
