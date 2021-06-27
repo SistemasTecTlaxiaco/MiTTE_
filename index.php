576e5de2e8c68fa6257b35aacb0354e13153f1a7
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-06-27 01:22:30 -0500

    Eliminación de rutas PHPMailer

--- index.php
+++ index.php
@@ -11,11 +11,7 @@ require_once 'libs/app.php';
 include_once 'includes/user_session.php';
 include_once 'includes/sessionController.php';
 require_once 'config/config.php';
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
 $app = new App();
-
 $nemail = new PHPMailer(true);
 
 
