cc52fdf4f15399e7c7a1da33023ead2a09a620d8
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-10-13 01:03:17 -0500

    Update header.php

--- views/principal/header.php
+++ views/principal/header.php
@@ -29,13 +29,14 @@
                     <li class="nav-item"><a class="nav-link" href="<?php echo constant('URL');?>posgrados">Posgrados</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo constant('URL');?>proyectos">Proyectos</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo constant('URL');?>nuevoArchivo">Subir Archivo</a></li>         
-                </ul> 
-            </div>
+                </ul>               
+                </div>
             <div class="dropdown me-2">
             <form class="d-sm-inline-block" action="<?php echo constant('URL'); ?>busqueda/buscar" enctype="multipart/form-data" method="POST">
             <div class="input-group input-group-navbar">
       <input class="form-control" name="buscar" type="Search" placeholder="Buscar archivo" aria-label="Buscar">
       <button class="btn  btn-icon btn-outline-light" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search align-middle"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></button>
+</div>
 </div>
             <div class="dropdown">
       <a href="#" class="d-flex align-items-center text-white text-decoration-none" data-toggle="collapse" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
@@ -65,7 +66,7 @@
                     $notifica=$noti; 
                     
      ?>     
-        <li><a class="dropdown-item" href="<?php echo constant('URL').$notifica->enlace;?>">El usuario <?php echo $notifica->usuario;?> ha <?php echo $notifica->tipo;?></a></li>
+        <li><a class="dropdown-item" href="<?php echo constant('URL').$notifica->enlace;?>" El usuario <?php echo $notifica->usuario;?> ha <?php echo $notifica->tipo;?></a></li>
         <?php }
         }
     ?>            
