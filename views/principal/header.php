63d3f8d73e9bee3b29084257bfc300f24ab7f7dd
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-10-13 01:05:43 -0500

    Update header.php

--- views/principal/header.php
+++ views/principal/header.php
@@ -37,7 +37,7 @@
       <input class="form-control" name="buscar" type="Search" placeholder="Buscar archivo" aria-label="Buscar">
       <button class="btn  btn-icon btn-outline-light" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search align-middle"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></button>
 </div>
-            </div>
+</div>
             <div class="dropdown">
       <a href="#" class="d-flex align-items-center text-white text-decoration-none" data-toggle="collapse" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="<?php echo constant('URL');?>/img/mensaje.svg" alt="" width="32" height="32" class="rounded-circle me-2">        
