30ffbbc6f79dec22b211ad218eac6094479a84ca
antoniio <79992182+antoniio@users.noreply.github.com>
2021-11-22 12:47:10 -0600

    Eliminacion de codigo basura en posgrados

--- views/posgrados/index.php
+++ views/posgrados/index.php
@@ -39,8 +39,6 @@
   <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -51,31 +49,7 @@
 proyectos educativos.</p>
       </div>
     </div>
-    <div class="carousel-item">
-      <img src="<?php echo constant('URL'); ?>img/sl2.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Comparte tus conocimientos</h5>
-        <p>Con MiTTE puedes compartir todos tus proyectos educativos.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="<?php echo constant('URL'); ?>img/sl3.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Forma parte de la comunidad</h5>
-        <p>Forma parte de nuestra comunidad para colaborar con correcciones y recibir ayuda de la comunidad.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previo</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Siguiente</span>
-  </button>
-</div>
-  </section>
+
 
   <div class="album py-5">
     <div class="container">
