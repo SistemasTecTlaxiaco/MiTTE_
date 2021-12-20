385b4628840f9ea3a18537509061fbbd520c01ca
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2021-12-20 15:44:41 -0600

    correcion slider2

--- views/posgrados/index.php
+++ views/posgrados/index.php
@@ -41,14 +41,12 @@
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
 
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item">
+  <div class="carousel-inner">   
       <img src="<?php echo constant('URL'); ?>img/sl5.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Descripcion</h5>
         <p>En este apartado encontraras una variedad de investigaciones y tesis echas por estudiantes universitarios (Ingenieria y licenciatura).</p>
-      </div>
-    </div> 
+      </div>    
   </section>
   <div class="album py-5">
     <div class="container">
